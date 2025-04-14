017f6b0973b60c978eff198919f35f571650fb53
trantdat <dattthe172813@fpt.edu.vn>
2025-04-15 03:03:50 +0700

    select mode customize

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,60 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import {
-  
-  useColorScheme
-} from '@mui/material/styles'
+import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SystemBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import { Box } from '@mui/material'
 
+
+function ModeSelect() {
+
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SystemBrightnessIcon fontSize='small' />
+            System
+          </Box>
+
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -28,8 +77,10 @@ function App() {
 
   return (
     <>
-    <ModeToggle />
-    <hr />
+      <ModeSelect></ModeSelect>
+      <hr />
+      <ModeToggle />
+      <hr />
       <div>Trantdat</div>
 
       <Typography variant='body2' color='text.secondary'>Yasuo</Typography>
